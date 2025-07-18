681d8480fe580a02c5b1f2a3674ecc85ec7fa0a7
Febryand Nur Hidayat <154643861+ryandnurh@users.noreply.github.com>
2025-07-18 18:34:11 +0700

    Update UserController.php

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -34,7 +34,7 @@ class UserController extends Controller
     {
         // Validasi input
         $request->validate([
-            'email' => 'email|max:100|unique:tb_users,email,'.auth()->user(),
+            'email' => 'email|max:100|unique:tb_users,email,'.auth()->id_user(),
         ]);
 
         // Ambil user yang sedang login
